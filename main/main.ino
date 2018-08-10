150acac0f3cbcb8db41503c571d72bdc49dbf941
Trent Rand <trand@squarespace.com>
2018-08-10 10:27:33 -0400

    Read volume using analog potentiometer
    The analog potentiometer now manipulates the volume variable exactly as it did when it was connected to the original PCB; however, this value is not being output.

--- main/main.ino
+++ main/main.ino
@@ -3,10 +3,18 @@
 */
 
 #include <Arduino.h>
+#include "AnalogPot.h";
+
+int volume = 0;
+
+AnalogPot analogPot(5);  // input pin for the potentiometer
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
+  volume = analogPot.read();
+  delay(50);
+  Serial.println(volume);
 }
