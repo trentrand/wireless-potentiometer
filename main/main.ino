e298ba61a40f1a3b921a9df36fe99b5e7493fc3b
Trent Rand <trand@squarespace.com>
2018-08-10 10:28:04 -0400

    Output volume of analog potentiometer using the MCP41100

--- main/main.ino
+++ main/main.ino
@@ -3,10 +3,13 @@
 */
 
 #include <Arduino.h>
+#include "MCP41100.h";
 #include "AnalogPot.h";
 
+byte address = 0x11;
 int volume = 0;
 
+MCP41100 digitalPot(10); // LOW = receive commands from SI_PIN
 AnalogPot analogPot(5);  // input pin for the potentiometer
 
 void setup() {
@@ -17,4 +20,5 @@ void loop() {
   volume = analogPot.read();
   delay(50);
   Serial.println(volume);
+  digitalPot.write(map(volume, 0, 1024, 0, 255));
 }
