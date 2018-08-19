8f38eb7e38312c3293284bab91254cbc84f54c3a
Trent Rand <trand@squarespace.com>
2018-08-18 20:32:03 -0400

    Add utilities for pairing IR commands

--- main/InfaredReceiver.cpp
+++ main/InfaredReceiver.cpp
@@ -24,4 +24,58 @@ Command InfaredReceiver::read() {
   }
 
   delay(100);
+  return -1; // no command received, don't register this read.
 }
+
+Command InfaredReceiver::registerCommand(byte command) {
+  Command commandsReceived[COMMANDS_TO_CONFIRM];
+
+  for (byte count = 0; count < COMMANDS_TO_CONFIRM;) {
+    Command command = read();
+
+    if (command != REPEAT && command != -1) {
+      Serial.print("Received potential command: ");
+      Serial.println(command, DEC);
+
+      commandsReceived[count] = command;
+      count++;
+    }
+  }
+
+  return getMostFrequentCommand(commandsReceived);
+}
+
+static int InfaredReceiver::comparator(const void *cmp1, const void *cmp2) {
+  Command a = *((Command *)cmp1);
+  Command b = *((Command *)cmp2);
+  return b - a;
+}
+
+Command InfaredReceiver::getMostFrequentCommand(Command commands[]) {
+  Command prevCommand = commands[0]; // 0
+  byte currentCount = 1; // c1 c2
+
+  Command mostFrequentCommand;
+  byte maxCount;
+
+  qsort(commands, COMMANDS_TO_CONFIRM, sizeof(commands[0]), comparator);
+
+
+  for (byte i = 1; i < COMMANDS_TO_CONFIRM; i++) {
+    if (commands[i] == prevCommand) {
+      currentCount++;
+    } else {
+      if (currentCount > maxCount) {
+        mostFrequentCommand = prevCommand;
+        maxCount = currentCount;
+      }
+
+      prevCommand = commands[i];
+      currentCount = 0;
+    }
+  }
+
+  return mostFrequentCommand;
+}
+
+
--- main/InfaredReceiver.h
+++ main/InfaredReceiver.h
@@ -8,11 +8,17 @@ typedef long unsigned int Command;
 
 #include "IRremote.h"
 
+#define COMMANDS_TO_CONFIRM 3
+
 class IRRecv;
+
 class InfaredReceiver {
   public:
     InfaredReceiver(int pin);
     Command read();
+    Command registerCommand(byte command);
+    static int comparator(const void *cmp1, const void *cmp2);
+    Command getMostFrequentCommand(Command commands[]);
   private:
     int _pin;
     decode_results results;
