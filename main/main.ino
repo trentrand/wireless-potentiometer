7df1c7ac29e533c096f4e00220a6b2c054b587b8
Trent Rand <trand@squarespace.com>
2018-08-19 18:25:24 -0400

    Initialize tranducer controllers in setup
    Before this, the controller classes could not use Serial in their initializers, because main hadn't yet call begin. (line 30)

--- main/main.ino
+++ main/main.ino
@@ -13,17 +13,21 @@ const int RECEIVER_PIN = 3;
 
 int volume = 0;
 
-MCP41100 digitalPot(DIGITAL_POT_PIN);
-AnalogPot analogPot(ANALOG_PIN);
-InfaredReceiver irReceiver(RECEIVER_PIN);
+AnalogPot* analogPot;
+InfaredReceiver* infaredReceiver;
+MCP41100* digitalPot;
 
 void setup() {
   Serial.begin(9600);
+
+  analogPot = new AnalogPot(ANALOG_PIN);
+  infaredReceiver = new InfaredReceiver(RECEIVER_PIN);
+  digitalPot = new MCP41100(DIGITAL_POT_PIN);
 }
 
 void loop() {
-  volume = analogPot.read();
+  volume = analogPot->read();
   delay(50);
   Serial.println(volume);
-  digitalPot.write(map(volume, 0, 1024, 0, 255));
+  digitalPot->write(map(volume, 0, 1024, 0, 255));
 }
