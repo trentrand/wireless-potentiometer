42a9a027c78f9c4c487466838e785ff10cc0d0d7
Trent Rand <trand@squarespace.com>
2018-08-19 22:39:34 -0400

    Run IR pairing wizard on first boot

--- main/main.ino
+++ main/main.ino
@@ -17,6 +17,9 @@ struct Config {
   Command volumeDownCode;
 } config;
 
+const byte VOLUME_UP = 1;
+const byte VOLUME_DOWN = -1;
+
 int volume = 0;
 
 AnalogPot* analogPot;
@@ -31,6 +34,12 @@ void setup() {
   digitalPot = new MCP41100(DIGITAL_POT_PIN);
 
   volume = analogPot->read();
+
+  config = EEPROM.get(0, config);
+  
+  if (config.volumeUpCode == 0 || config.volumeDownCode == 0) {
+    pair();
+  }
 }
 
 void loop() {
@@ -39,3 +48,21 @@ void loop() {
   Serial.println(volume);
   digitalPot->write(map(volume, 0, 1024, 0, 255));
 }
+
+void pair() {
+  Serial.println("Before using this device, you'll need to pair your IR remote.\n");
+
+  Serial.println("Let's start by pairing the Volume Up button on your remote. Press it a few times until you see a green light.");
+  Serial.println("Press the Volume Up button once more to confirm.\n");
+  config.volumeUpCode = infaredReceiver->registerCommand(VOLUME_UP);
+
+  delay(2000);
+  
+  Serial.println("Great! Now we'll pair the Volume Down button. Press it a few times until you see a green light.");
+  Serial.println("Press the Volume Down button once more to confirm.\n");
+  config.volumeDownCode = infaredReceiver->registerCommand(VOLUME_DOWN);
+
+  EEPROM.put(0, config);
+  Serial.println("Your remote has been successfully paired. To reset this pairing, press the reset button on your device.\n");
+}
+
